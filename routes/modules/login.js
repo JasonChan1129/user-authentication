5e1b421ff0fe63b62175ecc868d35553387bbd7e
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-21 18:21:22 +0800

    feat: modify home route and login route

--- routes/modules/login.js
+++ routes/modules/login.js
@@ -2,14 +2,16 @@ const express = require('express');
 const User = require('../../models/Users');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+	res.render('login');
+});
+
 router.post('/', (req, res) => {
 	const { email, password } = req.body;
 	User.findOne({ email, password })
 		.lean()
 		.then(user => {
-			return user
-				? res.render('welcome', { user })
-				: res.render('welcome', { error: 'Invalid username or password.' });
+			return user ? res.render('home', { user }) : res.render('error');
 		})
 		.catch(error => console.log(error));
 });
