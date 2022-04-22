37b2e105f9eda1a379c082d9bed2e7e22f929d9f
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-22 15:48:32 +0800

    feat: add retain user login record

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 const routes = require('./routes');
@@ -11,6 +12,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(routes);
 
 const PORT = 3000;
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Session = require('../../session');
+const User = require('../../models/Users');
 
-router.get('/', (req, res) => {
-	res.render('home');
+router.get('/', async (req, res) => {
+	try {
+		const sessionID = req.cookies.session_id;
+		const userSession = Session[sessionID];
+		if (userSession) {
+			const user = await User.findOne({ id: userSession.user_id }).lean();
+			if (user) {
+				return res.render('home', { user });
+			} else {
+				return res.render('home');
+			}
+		}
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 module.exports = router;
