d3e75dfc56750d40eb6dcc66c96ed0a4c8925440
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-22 13:18:22 +0800

    feat: add logout

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const home = require('../routes/modules/home');
 const login = require('../routes/modules/login');
+const logout = require('../routes/modules/logout');
 
 router.use('/', home);
 router.use('/login', login);
+router.use('/logout', logout);
 
 module.exports = router;
